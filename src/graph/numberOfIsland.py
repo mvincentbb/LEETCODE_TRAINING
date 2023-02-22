05c88508cf72b52cf0ffe595074ad46f555cfc25
vincent <vincent.bakpatina@ipnetinstitute.com>
2023-02-22 23:36:44 +0000

    fix deque attribute

--- src/graph/numberOfIsland.py
+++ src/graph/numberOfIsland.py
@@ -2,6 +2,7 @@ import collections
 
 
 class Solution:
+    #implement the solution
     def numIslands(self, grid: List[List[str]]) -> int:
         if not grid:
             return 0
@@ -10,7 +11,7 @@ class Solution:
         rows, cols = len(grid), len(grid[0])
         visit = set()
         islands = 0
-
+        # BFS
         def bfs(r,c):
             q = collections.deque()
             visit.add((r,c))
